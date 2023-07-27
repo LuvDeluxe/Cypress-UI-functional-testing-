152e7cd55d654056149843e748bc2bac35f474fb
Simon Simeonov <simeon.p.simeonov92@gmail.com>
2023-07-27 18:50:04 +0800

    Added dot env + unlucky path for login

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,14 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require("cypress")
+require('dotenv').config()
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    env: {
+      loginUsername: process.env.LOGIN_USERNAME,
+      loginPassword: process.env.LOGIN_PASSWORD
+    }
   },
 });
---
+++ cypress/e2e/swaglabs/loginpage/login.cy.js
@@ -0,0 +1,34 @@
+describe('Should verify the unlucky path for login', () => {
+  beforeEach(() => {
+    cy.visit('https://www.saucedemo.com/')
+  })
+
+  it('Verifies that pressing login without username / password will prompt errors. Pressing X clears the errors', () => {
+    cy.get('#login-button').click()
+    cy.get('#user-name').should('have.class', 'error')
+    cy.get('#password').should('have.class', 'error')
+    cy.get('.error').should('exist').contains('Epic sadface: Username is required')
+    cy.get('.error-button', { timeout: 10000 }).should('be.visible').click()
+    cy.get('.error').should('not.exist')
+  })
+
+  it('Verifies that entering only username and no password will trigger error', () => {
+    cy.get('input[name="user-name"]').click().clear().type(Cypress.env('loginUsername'), { log: false })
+    cy.get('#login-button').click()
+    cy.get('input[name="user-name"]').should('have.class', 'error')
+    cy.get('#password').should('have.class', 'error')
+    cy.get('.error').should('exist').contains('Epic sadface: Password is required')
+    cy.get('.error-button', { timeout: 10000 }).should('be.visible').click()
+    cy.get('.error').should('not.exist')
+  })
+
+  it('Verifies that entering only password and no username will trigger error', () => {
+    cy.get('#password').click().clear().type(Cypress.env('loginPassword'), { log: false })
+    cy.get('#login-button').click()
+    cy.get('input[name="user-name"]').should('have.class', 'error')
+    cy.get('#password').should('have.class', 'error')
+    cy.get('.error').should('exist').contains('Epic sadface: Username is required')
+    cy.get('.error-button', { timeout: 10000 }).should('be.visible').click()
+    cy.get('.error').should('not.exist')
+  })
+})
